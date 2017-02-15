305037b7ecf201c6ac6258bf3c5a758058fb445f
Teis Lindemark <teis.lindemark@gmail.com>
2017-02-15 22:41:32 +0100

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Stimulsoft.Report.NetCore;
+
+namespace MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnviroment;
+
+        public HomeController(IHostingEnvironment hostingEnviroment)
+        {
+            _hostingEnviroment = hostingEnviroment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult GetReport()
+        {
+            var reportPath = _hostingEnviroment.WebRootPath + "\\Reports\\SimpleList.mrt";
+            return StiNetCoreViewer.GetReportResult(this, reportPath);
+        }
+        
+        public IActionResult ViewerEvent()
+        {
+            return StiNetCoreViewer.ViewerEventResult(this);
+        }
+    }
+}
